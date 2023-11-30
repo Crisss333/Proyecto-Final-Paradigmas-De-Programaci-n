8fdbf2ebc7d9f6474fb79572b26d97dade78d9f5
Crisss333 <142540149+Crisss333@users.noreply.github.com>
2023-11-30 11:05:32 -0300

    api.py

--- BACKEND/api.py
+++ BACKEND/api.py
@@ -1,109 +1,92 @@
 from flask import Flask, jsonify, render_template
 import pandas as pd
-from collections import defaultdict
+
+class TitanicDataHandler:
+    def _init_(self, file_path):
+        self.df = pd.read_csv(file_path)
+
+    def get_survivors(self):
+        return self.df[self.df['Survived'] == 1].to_dict(orient='records')
+
+    def get_survival_stats(self):
+        survival_counts = self.df['Survived'].value_counts().to_dict()
+        return {'Survived': survival_counts.get(1, 0), 'Did not survive': survival_counts.get(0, 0)}
+
+    def get_non_survivors(self):
+        return self.df[self.df['Survived'] == 0].to_dict(orient='records')
+
+    def get_embark_stats(self):
+        embark_survived = self.df.groupby('Embarked')['Survived'].sum().to_dict()
+        embark_not_survived = self.df.groupby('Embarked')['Survived'].apply(lambda x: (x==0).sum()).to_dict()
+        return {'embark_survived': embark_survived, 'embark_not_survived': embark_not_survived}
+
+    def get_age_distribution(self):
+        bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, float('inf')]
+        labels = ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+']
+        self.df['AgeGroup'] = pd.cut(self.df['Age'], bins=bins, labels=labels, right=False)
+        age_dist = self.df.groupby(['AgeGroup', 'Survived']).size().unstack(fill_value=0)
+
+        age_labels = list(age_dist.index)
+        survivors = age_dist[1].tolist()
+        non_survivors = age_dist[0].tolist()
+
+        return {
+            'age_labels': age_labels,
+            'survivors': survivors,
+            'non_survivors': non_survivors
+        }
+
+    def get_fare_stats(self):
+        fare_ranges = [0, 25, 50, 100, 200, 300, 400, 500, 600, float('inf')]
+        labels = ['0-24', '25-49', '50-99', '100-199', '200-299', '300-399', '400-499', '500-599', '600+']
+        self.df['FareGroup'] = pd.cut(self.df['Fare'], bins=fare_ranges, labels=labels, right=False, include_lowest=True)
+        fare_dist = self.df.groupby(['FareGroup', 'Survived']).size().unstack(fill_value=0)
+
+        fare_labels = list(fare_dist.index)
+        fare_survived = fare_dist[1].tolist()
+        fare_not_survived = fare_dist[0].tolist()
+
+        return {
+        'fare_labels': fare_labels,
+        'fare_survived': fare_survived,
+        'fare_not_survived': fare_not_survived
+    }
 
 app = Flask(_name_)
+data_handler = TitanicDataHandler(r'C:\Users\Acer\Desktop\MiProyectoTitanic\Backend\tested.csv')  # Reemplaza con la ruta correcta de tu archivo CSV
 
-# Cargar el dataset del Titanic
-df = pd.read_csv(r'C:\Users\Acer\Desktop\MiProyectoTitanic\Backend\tested.csv')
-
-@app.route('/chart')
-def chart():
-    return render_template('front.html')
-
+# Rutas para el API
 @app.route('/')
 def home():
     return "Bienvenido a la API del Titanic!"
 
+@app.route('/chart')
+def chart():
+    return render_template('front.html')  # Asegúrate de que tu archivo HTML se llame 'index.html' y esté en la carpeta 'templates'
+
 @app.route('/survivors')
 def survivors():
-    survivors_data = df[df['Survived'] == 1]
-    return jsonify(survivors_data.to_dict(orient='records'))
+    return jsonify(data_handler.get_survivors())
 
 @app.route('/survival-stats')
 def survival_stats():
-    survival_counts = df['Survived'].value_counts().to_dict()
-    data = {
-        'Survived': survival_counts.get(1, 0),
-        'Did not survive': survival_counts.get(0, 0)
-    }
-    return jsonify(data)
+    return jsonify(data_handler.get_survival_stats())
 
 @app.route('/non-survivors')
 def non_survivors():
-    non_survivors_data = df[df['Survived'] == 0]
-    return jsonify(non_survivors_data.to_dict(orient='records'))
+    return jsonify(data_handler.get_non_survivors())
 
 @app.route('/embark-stats')
 def embark_stats():
-    embark_survived = df.groupby('Embarked')['Survived'].sum().to_dict()
-    embark_not_survived = df.groupby('Embarked')['Survived'].apply(lambda x: (x==0).sum()).to_dict()
-    data = {
-        'embark_survived': embark_survived,
-        'embark_not_survived': embark_not_survived
-    }
-    return jsonify(data)
+    return jsonify(data_handler.get_embark_stats())
 
 @app.route('/age-distribution')
 def age_distribution():
-    # Define los rangos de edades y las etiquetas correspondientes
-    bins = [0, 10, 20, 30, 40, 50, 60, 70, 80, float('inf')]
-    labels = ['0-9', '10-19', '20-29', '30-39', '40-49', '50-59', '60-69', '70-79', '80+']
-
-    # Utiliza pd.cut para segmentar las edades en los rangos definidos
-    df['AgeGroup'] = pd.cut(df['Age'], bins=bins, labels=labels, right=False)
-
-    # Agrupa por 'AgeGroup' y 'Survived' y cuenta las entradas
-    age_dist = df.groupby(['AgeGroup', 'Survived']).size().unstack(fill_value=0)
-    
-    # Añade el conteo de nulos si es necesario
-    null_survived = int(df[df['Age'].isnull() & (df['Survived'] == 1)].shape[0])
-    null_not_survived = int(df[df['Age'].isnull() & (df['Survived'] == 0)].shape[0])
-    if null_survived or null_not_survived:
-        age_dist.loc['Nulos', 1] = null_survived
-        age_dist.loc['Nulos', 0] = null_not_survived
-
-    # Prepara los datos para el gráfico
-    age_labels = list(age_dist.index)
-    survivors = age_dist[1].tolist()
-    non_survivors = age_dist[0].tolist()
-
-    return jsonify({
-        'age_labels': age_labels,
-        'survivors': survivors,
-        'non_survivors': non_survivors
-    })
+    return jsonify(data_handler.get_age_distribution())
 
 @app.route('/fare-stats')
 def fare_stats():
-    # Aquí se deben crear rangos para los precios de los tickets y calcular los sobrevivientes y no sobrevivientes
-    fare_ranges = [0, 25, 50, 100, 200, 300, 400, 500, 600, 'Nulos']
-    fare_distribution = {
-        str(fare_range): {
-            'survivors': 0,
-            'non_survivors': 0
-        } for fare_range in fare_ranges
-    }
-
-    for _, row in df.iterrows():
-        fare = row['Fare']
-        survived = row['Survived']
-        if pd.isna(fare):
-            label = 'Nulos'
-        else:
-            label = str(max([f for f in fare_ranges if f != 'Nulos' and f <= fare], default=0))
-        if survived:
-            fare_distribution[label]['survivors'] += 1
-        else:
-            fare_distribution[label]['non_survivors'] += 1
-    
-    # Prepara los datos para enviar al frontend
-    data = {
-        'fare_labels': [str(f) for f in fare_ranges],
-        'fare_survived': [fare_distribution[str(f)]['survivors'] for f in fare_ranges],
-        'fare_not_survived': [fare_distribution[str(f)]['non_survivors'] for f in fare_ranges]
-    }
-    return jsonify(data)
+    return jsonify(data_handler.get_fare_stats())
 
 if _name_ == '_main_':
     app.run(debug=True)
